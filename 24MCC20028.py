1a733dc0a42a11bfadff754ea49809493300d514
Sheetal Yadav <154402597+SheetalYadav79@users.noreply.github.com>
2025-07-20 12:43:21 +0530

    Update 24MCC20028.py

--- 24MCC20028.py
+++ 24MCC20028.py
@@ -1,2 +1,3 @@
 print("Hey, I am Sheetal and i am doing my frst experiment of DevOps")
 print("Hello, I am updating this file for the 3rd part of my experiment of DevOps Process Automation Lab")
+print("Hi")
